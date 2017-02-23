39d94febe61a8ead8d02938d35b8d311b9d22000
Jeremy Trufier <jeremy@trufier.com>
2017-02-23 13:01:47 +0100

    Reject unsupported methods

---
+++ test/unit/crud/lib.ts
@@ -0,0 +1,19 @@
+export function respondJson(route: string, content?: any, statusCode?: number): any {
+  let [method, endpoint] = route.split(' ')
+  let reqPointer: any = {};
+
+  this.respondWith(method, `api/${endpoint}.json`, function(request: any) {
+    let body = null;
+    try { body = JSON.parse(request.requestBody); } catch (e) {}
+    reqPointer.body = body;
+    reqPointer.headers = request.requestHeaders;
+
+    request.respond(
+      statusCode || 200,
+      { 'Content-Type': 'application/json' },
+      JSON.stringify(content || null)
+    );
+  });
+
+  return reqPointer;
+}
